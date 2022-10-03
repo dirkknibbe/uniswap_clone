6a8218d508aa583acfd908d7c794201f9e27f7f6
dirkknibbe <dirk.knibbe@yahoo.com>
2022-10-03 18:07:47 -0500

    started amountin amountout and balance file structure for the exchange page

--- packages/react-app/src/components/Exchange.js
+++ packages/react-app/src/components/Exchange.js
@@ -12,9 +12,21 @@ import { ethers } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
 
 import { ROUTER_ADDRESS } from "../config";
+import { AmountIn, AmountOut, Balance } from "./";
 
 const Exchange = ({ pools }) => {
-  return <div>Exchange</div>;
+  return (
+    <div className="flex flex-col w-full items-center">
+      <div className="mb-8">
+        <AmountIn />
+        <Balance />
+      </div>
+      <div className="mb-8 w-[100%]">
+        <AmountOut />
+        <Balance />
+      </div>
+    </div>
+  );
 };
 
 export default Exchange;
