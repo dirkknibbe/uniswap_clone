2102e50e8ebecbb8fe270281427834f25d71309a
dirkknibbe <dirk.knibbe@yahoo.com>
2022-10-03 13:46:59 -0500

    created walletbutton loader and exchange components and components folder

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -4,6 +4,10 @@ import styles from "./styles";
 import { uniswapLogo } from "./assets";
 
 const App = () => {
+  const { account } = useEthers();
+
+  const poolsLoading = false;
+
   return (
     <div className={styles.container}>
       <div className={styles.innerContainer}>
@@ -13,7 +17,7 @@ const App = () => {
             alt="uniswap logo"
             className="w-16 h-16 object-contain"
           />
-          WalletButton
+          <WalletButton />
         </header>
         <div className={styles.exchangeContainer}>
           <h1 className={styles.headTitle}>Uniswap 2.0</h1>
@@ -22,7 +26,17 @@ const App = () => {
           <div className={styles.exchangeBoxWrapper}>
             <div className={styles.exchangeBox}>
               <div className="pink_gradient" />
-              <div className={styles.exchange}></div>
+              <div className={styles.exchange}>
+                {account ? (
+                  poolsLoading ? (
+                    <Loader />
+                  ) : (
+                    <Exchange />
+                  )
+                ) : (
+                  <Loader />
+                )}
+              </div>
               <div className="blue_gradient" />
             </div>
           </div>
