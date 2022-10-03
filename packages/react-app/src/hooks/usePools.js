723c762cd3f9ab7a2374c45d933b451bccfe9fb5
dirkknibbe <dirk.knibbe@yahoo.com>
2022-10-03 16:37:38 -0500

    created file structure for usePools hook with util helper functions files

--- packages/react-app/src/hooks/usePools.js
+++ packages/react-app/src/hooks/usePools.js
@@ -4,8 +4,25 @@ import { useConfig } from "@usedapp/core";
 
 import { ROUTER_ADDRESS } from "../config";
 
+export const loadPools = async (providerUrl) => {
+  const provider = new Web3.providers.HttpProvider(providerUrl);
+  const web3 = new Web3(provider);
+
+  const routerInfo = null;
+  const factoryInfo = null;
+};
+
 export const usePools = () => {
   const { readOnlyChainId, readOnlyUrls } = useConfig();
   const [loading, setLoading] = useState(true);
   const [pools, setPools] = useState({});
+
+  useEffect(() => {
+    loadPools(readOnlyUrls[readOnlyChainId]).then((pools) => {
+      setPools(pools);
+      setLoading(false);
+    });
+  }, [readOnlyChainId, readOnlyUrls]);
+
+  return [loading, pools];
 };
