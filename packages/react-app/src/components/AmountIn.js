91fa1c72520fc3ea13b3738b95fa798ef6a331cd
dirkknibbe <dirk.knibbe@yahoo.com>
2022-10-04 15:35:15 -0500

    finished basic ui for amountin component

--- packages/react-app/src/components/AmountIn.js
+++ packages/react-app/src/components/AmountIn.js
@@ -5,6 +5,8 @@ import styles from "../styles";
 import { useOnClickOutside } from "../utils";
 
 const AmountIn = () => {
+  const [showList, setShowList] = useState(false);
+
   return (
     <div className={styles.amountContainer}>
       <input
@@ -15,6 +17,38 @@ const AmountIn = () => {
         onChange={() => {}}
         className={styles.amountInput}
       />
+      <div
+        className="relative"
+        onClick={() => setShowList((prevState) => !prevState)}
+      >
+        <button className={styles.currencyButton}>
+          {"ETH"}
+          <img
+            src={chevronDown}
+            alt="chevron down"
+            className={`w-4 h-4 object-contain ml-2 ${
+              showList ? "rotate-180" : "rotate-0"
+            }`}
+          />
+        </button>
+        {showList && (
+          <ul className={styles.currencyList}>
+            {[
+              { token: "ETH", tokenName: "ETH" },
+              { token: "RMR", tokenName: "Remora Governance" },
+            ].map(({ token, tokenName }, index) => (
+              <li
+                key={index}
+                className={`${styles.currencyListItem} ${
+                  true ? "bg-site-dim2" : ""
+                } cursor-pointer`}
+              >
+                {tokenName}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
