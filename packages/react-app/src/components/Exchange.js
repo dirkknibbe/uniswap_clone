c2229a5c5f5923608cd0e629150b326d492f806e
dirkknibbe <dirk.knibbe@yahoo.com>
2022-10-04 16:51:03 -0500

    basic exchange UI complete

--- packages/react-app/src/components/Exchange.js
+++ packages/react-app/src/components/Exchange.js
@@ -24,6 +24,12 @@ import {
 } from "../utils";
 
 const Exchange = ({ pools }) => {
+  const isApproving = isOperationPending("approve"); // TODO
+  const isSwapping = isOperationPending("swap"); // TODO
+
+  // const successMessage = getSuccessMessage(); // TODO
+  // const failureMessage = getFailureMessage(); // TODO
+
   return (
     <div className="flex flex-col w-full items-center">
       <div className="mb-8">
@@ -34,6 +40,43 @@ const Exchange = ({ pools }) => {
         <AmountOut />
         <Balance />
       </div>
+      {"approveNeeded" && !isSwapping ? (
+        <button
+          disabled={!"canApprove"}
+          onClick={() => {}}
+          className={
+            "canApprove"
+              ? "bg-site-pink text-white"
+              : "bg-site-dim2 text-site-dim2"`${styles.actionButton}`
+          }
+        >
+          {isApproving ? "Approving..." : "Approve"}
+        </button>
+      ) : (
+        <button
+          disabled={!"canSwap"}
+          onClick={() => {}}
+          className={
+            "canSwap"
+              ? "bg-site-pink text-white"
+              : "bg-site-dim2 text-site-dim2"`${styles.actionButton}`
+          }
+        >
+          {isSwapping
+            ? "Swapping..."
+            : "hasEnoughBalance"
+            ? "Swap"
+            : "Insufficient Balance"}
+        </button>
+      )}
+
+      {"failureMessage" && !"resetState" ? (
+        <p className={styles.message}>{"failureMessage"}</p>
+      ) : "successMessage" ? (
+        <p className={styles.message}>{"successMessage"}</p>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
