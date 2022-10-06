f9e5613a6ecdab30a99599506f13a31f8d49d20d
dirkknibbe <dirk.knibbe@yahoo.com>
2022-10-06 16:13:03 -0500

    onclick's wired up

--- packages/react-app/src/components/Exchange.js
+++ packages/react-app/src/components/Exchange.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Contract } from "@ethersproject/contracts";
 import { abis } from "@my-app/contracts";
 import { ethers } from "ethers";
@@ -26,7 +26,7 @@ import {
 const Exchange = ({ pools }) => {
   const { account } = useEthers();
   const [fromValue, setFromValue] = useState("0");
-  const [fromToken, setfromToken] = useState(pools[0].token0Address);
+  const [fromToken, setFromToken] = useState(pools[0].token0Address);
   const [toToken, setToToken] = useState("");
   const [resetState, setResetState] = useState(false);
 
@@ -70,22 +70,85 @@ const Exchange = ({ pools }) => {
   const successMessage = getSuccessMessage(swapApproveState, swapExecuteState);
   const failureMessage = getFailureMessage(swapApproveState, swapExecuteState);
 
+  const onApproveRequested = () => {
+    swapApproveSend(ROUTER_ADDRESS, ethers.constants.MaxUint256);
+  };
+
+  const onSwapRequested = () => {
+    swapExecuteSend(
+      fromValueBigNumber,
+      0,
+      [fromToken, toToken],
+      account,
+      Math.floor(Date.now() / 1000) + 60 * 20
+    ).then(() => {
+      setFromValue("0");
+    });
+  };
+
+  const onFromValueChange = (value) => {
+    const trimmedValue = value.trim();
+
+    try {
+      if (trimmedValue) {
+        parseUnits(value);
+
+        setFromValue(value);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onFromTokenChange = (value) => {
+    setFromToken(value);
+  };
+
+  const onToTokenChange = (value) => {
+    setToToken(value);
+  };
+
+  useEffect(() => {
+    if (failureMessage || successMessage) {
+      setTimeout(() => {
+        setResetState(true);
+        setFromValue("0");
+        setToToken("");
+      }, 5000);
+    }
+  }, [failureMessage, successMessage]);
+
   return (
     <div className="flex flex-col w-full items-center">
       <div className="mb-8">
-        <AmountIn />
-        <Balance />
+        <AmountIn
+          value={fromValue}
+          onChange={onFromValueChange}
+          currencyValue={fromToken}
+          onSelect={onFromTokenChange}
+          currencies={availableTokens}
+          isSwapping={isSwapping && hasEnoughBalance}
+        />
+        <Balance tokenBalance={fromTokenBalance} />
       </div>
       <div className="mb-8 w-[100%]">
-        <AmountOut />
-        <Balance />
+        <AmountOut
+          fromToken={fromToken}
+          toToken={toToken}
+          amountIn={fromValueBigNumber}
+          pairContract={pairAddress}
+          currencyValue={toToken}
+          onSelect={onToTokenChange}
+          currencies={counterpartTokens}
+        />
+        <Balance tokenBalance={toTokenBalance} />
       </div>
-      {"approveNeeded" && !isSwapping ? (
+      {approveNeeded && !isSwapping ? (
         <button
-          disabled={!"canApprove"}
-          onClick={() => {}}
+          disabled={!canApprove}
+          onClick={onApproveRequested}
           className={`${
-            "canApprove"
+            canApprove
               ? "bg-site-pink text-white"
               : "bg-site-dim2 text-site-dim2"
           }${styles.actionButton}`}
@@ -94,26 +157,24 @@ const Exchange = ({ pools }) => {
         </button>
       ) : (
         <button
-          disabled={!"canSwap"}
-          onClick={() => {}}
+          disabled={!canSwap}
+          onClick={onSwapRequested}
           className={`${
-            "canSwap"
-              ? "bg-site-pink text-white"
-              : "bg-site-dim2 text-site-dim2"
+            canSwap ? "bg-site-pink text-white" : "bg-site-dim2 text-site-dim2"
           }${styles.actionButton}`}
         >
           {isSwapping
             ? "Swapping..."
-            : "hasEnoughBalance"
+            : hasEnoughBalance
             ? "Swap"
             : "Insufficient Balance"}
         </button>
       )}
 
-      {"failureMessage" && !"resetState" ? (
-        <p className={styles.message}>{"failureMessage"}</p>
-      ) : "successMessage" ? (
-        <p className={styles.message}>{"successMessage"}</p>
+      {failureMessage && !resetState ? (
+        <p className={styles.message}>{failureMessage}</p>
+      ) : successMessage ? (
+        <p className={styles.message}>{successMessage}</p>
       ) : (
         ""
       )}
